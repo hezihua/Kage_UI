4dc0f8b99f8d3d4e24b825eb6f414318487586b4
hezihua <2283462371@qq.com>
2026-01-05 10:05:56 +0800

    feat: 移除多余的icon

--- packages/kage-icon/src/icons/index.ts
+++ packages/kage-icon/src/icons/index.ts
@@ -15,30 +15,19 @@ export { CaretRightOutlined } from './CaretRightOutlined';
 export { UpCircleOutlined } from './UpCircleOutlined';
 
 // 提示建议性图标
-export { QuestionOutlined } from './QuestionOutlined';
-export { QuestionCircleOutlined } from './QuestionCircleOutlined';
 export { PlusOutlined } from './PlusOutlined';
-export { PlusCircleOutlined } from './PlusCircleOutlined';
 export { PauseOutlined } from './PauseOutlined';
 export { PauseCircleOutlined } from './PauseCircleOutlined';
 export { MinusOutlined } from './MinusOutlined';
 export { MinusCircleOutlined } from './MinusCircleOutlined';
 export { PlusSquareOutlined } from './PlusSquareOutlined';
 export { MinusSquareOutlined } from './MinusSquareOutlined';
-export { InfoOutlined } from './InfoOutlined';
 export { InfoCircleOutlined } from './InfoCircleOutlined';
-export { ExclamationOutlined } from './ExclamationOutlined';
-export { ExclamationCircleOutlined } from './ExclamationCircleOutlined';
 export { CloseOutlined } from './CloseOutlined';
-export { CloseCircleOutlined } from './CloseCircleOutlined';
-export { CloseSquareOutlined } from './CloseSquareOutlined';
 export { CheckOutlined } from './CheckOutlined';
 export { CheckCircleOutlined } from './CheckCircleOutlined';
 export { CheckSquareOutlined } from './CheckSquareOutlined';
 export { ClockCircleOutlined } from './ClockCircleOutlined';
-export { WarningOutlined } from './WarningOutlined';
-export { IssuesCloseOutlined } from './IssuesCloseOutlined';
-export { StopOutlined } from './StopOutlined';
 
 // 导入所有图标组件
 import { StepBackwardOutlined } from './StepBackwardOutlined';
@@ -53,30 +42,19 @@ import { CaretDownOutlined } from './CaretDownOutlined';
 import { CaretLeftOutlined } from './CaretLeftOutlined';
 import { CaretRightOutlined } from './CaretRightOutlined';
 import { UpCircleOutlined } from './UpCircleOutlined';
-import { QuestionOutlined } from './QuestionOutlined';
-import { QuestionCircleOutlined } from './QuestionCircleOutlined';
 import { PlusOutlined } from './PlusOutlined';
-import { PlusCircleOutlined } from './PlusCircleOutlined';
 import { PauseOutlined } from './PauseOutlined';
 import { PauseCircleOutlined } from './PauseCircleOutlined';
 import { MinusOutlined } from './MinusOutlined';
 import { MinusCircleOutlined } from './MinusCircleOutlined';
 import { PlusSquareOutlined } from './PlusSquareOutlined';
 import { MinusSquareOutlined } from './MinusSquareOutlined';
-import { InfoOutlined } from './InfoOutlined';
 import { InfoCircleOutlined } from './InfoCircleOutlined';
-import { ExclamationOutlined } from './ExclamationOutlined';
-import { ExclamationCircleOutlined } from './ExclamationCircleOutlined';
 import { CloseOutlined } from './CloseOutlined';
-import { CloseCircleOutlined } from './CloseCircleOutlined';
-import { CloseSquareOutlined } from './CloseSquareOutlined';
 import { CheckOutlined } from './CheckOutlined';
 import { CheckCircleOutlined } from './CheckCircleOutlined';
 import { CheckSquareOutlined } from './CheckSquareOutlined';
 import { ClockCircleOutlined } from './ClockCircleOutlined';
-import { WarningOutlined } from './WarningOutlined';
-import { IssuesCloseOutlined } from './IssuesCloseOutlined';
-import { StopOutlined } from './StopOutlined';
 
 // 图标映射表
 export const iconMap: Record<string, React.ComponentType<any>> = {
@@ -92,30 +70,19 @@ export const iconMap: Record<string, React.ComponentType<any>> = {
   CaretLeftOutlined,
   CaretRightOutlined,
   UpCircleOutlined,
-  QuestionOutlined,
-  QuestionCircleOutlined,
   PlusOutlined,
-  PlusCircleOutlined,
   PauseOutlined,
   PauseCircleOutlined,
   MinusOutlined,
   MinusCircleOutlined,
   PlusSquareOutlined,
   MinusSquareOutlined,
-  InfoOutlined,
   InfoCircleOutlined,
-  ExclamationOutlined,
-  ExclamationCircleOutlined,
   CloseOutlined,
-  CloseCircleOutlined,
-  CloseSquareOutlined,
   CheckOutlined,
   CheckCircleOutlined,
   CheckSquareOutlined,
   ClockCircleOutlined,
-  WarningOutlined,
-  IssuesCloseOutlined,
-  StopOutlined,
 };
 
 // 所有图标名称列表
